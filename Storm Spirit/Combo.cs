dc4c084257eeb19f6f7d4f8c5ae878dd8e6c7c7d
VickTheRock <BlecTager@ya.ru>
2017-09-11 20:25:34 +0300

    R logic.

--- Storm Spirit/Combo.cs
+++ Storm Spirit/Combo.cs
@@ -417,6 +417,7 @@
                 && !inUltBall
                 && !R.IsInAbilityPhase
                 && !R.IsChanneling
+                && !W.CanBeCasted()
                 && !ExUnit.IsChanneling(me)
                 && (Config.AutoOverload.Value
                     && buff
